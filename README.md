ec82ff47e6907c3ff0a3d9988b31887be66559dc
Derek Kwan <derek.x.kwan@gmail.com>
2018-07-22 06:10:13 -0700

    adding hash-nset

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ miscellaneous racket stuff that doesn't have a home yet
 
 ## dxk-hash-util.rkt
 - **hash-nref** - drill down a hash table by list of keys
+- **hash-nset** - nested hash table setting
 
 ## dxk-list-util.rkt
 - **chunkify** - splits a list into equal-sized sublists (with the leftovers)
--- dxk-hash-util.rkt
+++ dxk-hash-util.rkt
@@ -2,6 +2,7 @@
 
 (provide (all-defined-out))
 
+;; nested reference
 (define (hash-nref myhash mykeys [nope '()])
   (if (and (hash? myhash) (> (length mykeys) 0))
     (let* ([cur-key (car mykeys)]
@@ -12,3 +13,13 @@
 	  (hash-nref cur-val rest-keys nope))
       )
     nope))
+
+;; nested setting
+(define (hash-nset myhash mykeys val)
+  (let* ([cur-key (car mykeys)]
+	 [rest-keys (cdr mykeys)]
+	 [rest-len (length rest-keys)]
+	 [cur-val (hash-ref myhash cur-key (hash))])
+    (if (= 0 rest-len) (hash-set myhash cur-key val)
+	(hash-set myhash cur-key (hash-nset cur-val rest-keys val))
+	)))
